b4ae20ce8e8c22beda05f2edab3df1fa521830ba
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2022-12-09 19:34:32 -0800

    Convert `minecartevents` to require
    Also removed some blank lines that had whitespace.

--- src/minecartevents/modules/minecartevents.lua
+++ src/minecartevents/modules/minecartevents.lua
@@ -1,29 +1,32 @@
-os.loadAPI("apis/util")
-os.loadAPI("apis/events")
+local util = require("util")
+local events = require("events")
+local minecartevents = {}
 
 local eventHandlers = util.initializeGlobalTable("minecartEventHandlers")
 local lastMinecartEvent = util.initializeGlobalTable("lastMinecartEvent")
 
-function registerMinecartHandler(handler)
+function minecartevents.registerMinecartHandler(handler)
 	eventHandlers["minecart"] = handler
 end
 
-function handleMinecartEvent(eventName, detector, minecartType, minecartName, ...)
+function minecartevents.handleMinecartEvent(eventName, detector, minecartType, minecartName, ...)
 	local timestamp = os.clock()
 	-- The entire point of this library is to eliminate the duplicate minecarts events that the digital detector sometimes fires.
 	if lastMinecartEvent.minecartName == minecartName and lastMinecartEvent.minecartType == minecartType and (timestamp - lastMinecartEvent.timestamp) < 1 then
 		return
 	end
-	
+
 	-- Setting timers doesn't seem to work here (the timer event is never fired), so we use timestamps instead.
 	lastMinecartEvent.minecartName = minecartName
 	lastMinecartEvent.minecartType = minecartType
 	lastMinecartEvent.timestamp = timestamp
-	
+
 	local handler = eventHandlers["minecart"]
 	if handler ~= nil then
 		return handler(eventName, detector, minecartType, minecartName, ...)
 	end
 end
 
-events.registerHandler("minecart", handleMinecartEvent)
+events.registerHandler("minecart", minecartevents.handleMinecartEvent)
+
+return minecartevents
